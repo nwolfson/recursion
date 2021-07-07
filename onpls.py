63f4fc75be1a4a4616fe947e0d2a7290bd3b958b
nwolfson <86404629+nwolfson@users.noreply.github.com>
2021-07-07 16:27:12 +0300

    Delete onpls.py

--- onpls.py
+++
@@ -1,5 +0,0 @@
-import nidcpower
-with nidcpower.Session(resource_name='PXI1Slot3', channels=1, reset=False, options={'simulate': True}) as session:
-    session.voltage_level = 5.0 # Выставлять флоут либо делать через рэндж/авторендж
-    session.commit() # Включает БП с прописанными свойствами
-    print(session.measure_multiple())
